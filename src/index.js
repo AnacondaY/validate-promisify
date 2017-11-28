033281b019d9563dd7ea27abec5e5b6430fcc188
AnacondaY <oliver_young@qq.com>
2017-11-28 19:52:42 +0800

    删除polyfill

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import './polyfill';
 import { default as Validator } from './validator';
 export { default as Schema } from './schema';
 export { default as lang } from './lang';
--- src/polyfill.js
+++
@@ -1,7 +0,0 @@
-import Promise from 'es6-promise';
-
-if(typeof window !== 'undefined'){
-    window.Promise = Promise;
-}else{
-    global.Promise = Promise;
-}
